f78f5851ff2efd250b904c46798e6bd6dbfd6038
Vanchegs <manshin9300@gmail.com>
2025-06-09 01:50:58 +0300

    Enemy update

--- Assets/Internal/Codebase/Infrastructure/Factories/EnemyFactory.cs
+++ Assets/Internal/Codebase/Infrastructure/Factories/EnemyFactory.cs
@@ -1,18 +1,20 @@
-using System;
 using UnityEngine;
 
 namespace Internal.Codebase
 {
-    [Serializable]
     public class EnemyFactory : MonoBehaviour, IEnemyFactory
     {
         [SerializeField] private Enemy enemy; 
         [SerializeField] private EnemyConfig enemyConfig;
-        
+
+        private void Start()
+        {
+            CreateEnemy();
+        }
+
         public Enemy CreateEnemy()
         {
             enemy.Initialize(enemyConfig);
-            
             
             Instantiate(enemy);
 
