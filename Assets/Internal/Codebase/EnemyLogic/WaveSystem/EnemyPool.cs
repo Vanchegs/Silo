3995b21d1f6ac2d96f775804d17f16efaac7b59c
Vanchegs <manshin9300@gmail.com>
2025-08-11 22:19:53 +0300

    enemyPool update

--- Assets/Internal/Codebase/EnemyLogic/WaveSystem/EnemyPool.cs
+++ Assets/Internal/Codebase/EnemyLogic/WaveSystem/EnemyPool.cs
@@ -1,6 +1,32 @@
-using UnityEngine;
+using Internal.Codebase;
+using System.Collections.Generic;
 
-public class EnemyPool<T> where T : MonoBehaviour
+public class EnemyPool<T> where T : Enemy
 {
+    private List<T> enemies;
+    private EnemyFactory enemyFactory;
     
+    public EnemyPool(EnemyConfigsDictionary enemyConfigs)
+    {
+        enemies = new List<T>();
+        enemyFactory = new EnemyFactory(enemyConfigs);
+    }
+
+    public void InitPool(int poolSize, EnemyType enemyType)
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            CreateNewEnemy(enemyFactory.CreateEnemy(enemyType));
+        }
+    }
+
+    public void CreateNewEnemy<T>(T enemy) where T : Enemy
+    {
+        
+    }
+
+    public T TryGetEnemy(T enemy)
+    {
+        
+    }
 }
